e9852c298bdd1cde108dbc4f97fdaa4cbd725086
DOJesse <diogojesse@gmail.com>
2025-03-24 19:55:32 -0300

    Criado Docker, DB e Tabela

---
+++ app/Docker/postgres/init.sql
@@ -0,0 +1,28 @@
+-- DROP SCHEMA public;
+
+DROP SCHEMA IF EXISTS public CASCADE;
+
+CREATE SCHEMA public AUTHORIZATION pg_database_owner;
+
+-- DROP SEQUENCE public.tbl_measurements_id_measurements_seq;
+
+CREATE SEQUENCE public.tbl_measurements_id_measurements_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+CREATE TABLE public.tbl_measurements (
+	id integer NOT NULL DEFAULT nextval('public.tbl_measurements_id_measurements_seq'),
+	id_sensor numeric(4) NOT NULL,
+	ds_city varchar(100) NOT NULL,
+	dt_date date NOT NULL,
+	qt_pm25 numeric(15, 2) NULL,
+    qt_avg_humidity numeric(15, 2) NULL,
+    qt_avg_temp_c numeric(15, 2) NULL,
+    qt_max_wind_kph numeric(15, 2) NULL,
+    qt_total_precip_mm numeric(15, 2) NULL,
+    CONSTRAINT pk_tbl_measurements PRIMARY KEY (id)
+);
